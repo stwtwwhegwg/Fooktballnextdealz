d2bbae9ee5f483ab1813d34518a502b10776f735
stwtwwhegwg <abdelashalaby7@gmail.com>
2025-06-02 16:02:08 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/HomePage';
+import DownloadPage from './components/Download/DownloadPage';
+import NewsList from './components/News/NewsList';
+import GamesQuestsPage from './components/GamesQuests/GamesQuestsPage';
+import Header from './components/Common/Header';
+import Footer from './components/Common/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/download" element={<DownloadPage />} />
+        <Route path="/news" element={<NewsList />} />
+        <Route path="/games" element={<GamesQuestsPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
