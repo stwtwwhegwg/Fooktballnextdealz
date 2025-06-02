dd0325c6f98842564494976618ea84422a433a48
stwtwwhegwg <abdelashalaby7@gmail.com>
2025-06-02 16:02:55 +0200

    Create Src.js

---
+++ Src.js
@@ -0,0 +1,6 @@
+export const routes = [
+  { path: '/', name: 'Home' },
+  { path: '/download', name: 'Download' },
+  { path: '/news', name: 'News' },
+  { path: '/games', name: 'Games & Quizzes' }
+];
