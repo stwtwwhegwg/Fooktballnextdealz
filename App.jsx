5bb1dfd4f445a0de449f51ee16f23133b27a49f8
stwtwwhegwg <abdelashalaby7@gmail.com>
2025-06-02 17:16:03 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/HomePage';
 import DownloadPage from './components/Download/DownloadPage';
 import NewsList from './components/News/NewsList';
@@ -9,18 +9,19 @@ import Footer from './components/Common/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/download" element={<DownloadPage />} />
-        <Route path="/news" element={<NewsList />} />
-        <Route path="/games" element={<GamesQuestsPage />} />
-      </Routes>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/download" element={<DownloadPage />} />
+          <Route path="/news" element={<NewsList />} />
+          <Route path="/games" element={<GamesQuestsPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
